1935b66b9879634d6c7fe22d442ddc1ea0228f04
Joseph Collins <joecollins@Josephs-MBP.home>
2023-04-18 12:11:16 +0100

    set User Context and create custom hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,50 @@
-import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/logo.png';
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
+import { useCurrentUser } from '../contexts/CurrentUserContext';
 
 const NavBar = () => {
-  return (
-    <div>
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
-            <Container>
-                <NavLink to="/">
-                    <Navbar.Brand>
-                        < img src={logo} alt="logo" height="45" />
-                    </Navbar.Brand>
-                </NavLink>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto text-start">
-                        <NavLink
-                            exact
-                            className={styles.NavLink}
-                            activeClassName={styles.Active}
-                            to="/">
-                                <i className='fas fa-home'></i>Home
-                        </NavLink>
-                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
-                            <i className='fas fa-sign-in-alt'></i>Sign in
-                        </NavLink>
-                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
-                            <i className='fas fa-user-plus'></i>Sign up
-                        </NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    </div>
-  )
+    const currentUser = useCurrentUser();
+    const loggedInIcons = <>{currentUser?.username}</>
+    const loggedOutIcons = (
+    <>
+        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
+            <i className='fas fa-sign-in-alt'></i>Sign in
+        </NavLink>
+        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+            <i className='fas fa-user-plus'></i>Sign up
+        </NavLink>
+    </>
+    );
+
+    return (
+        <div>
+            <Navbar className={styles.NavBar} expand="md" fixed="top">
+                <Container>
+                    <NavLink to="/">
+                        <Navbar.Brand>
+                            < img src={logo} alt="logo" height="45" />
+                        </Navbar.Brand>
+                    </NavLink>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto text-start">
+                            <NavLink
+                                exact
+                                className={styles.NavLink}
+                                activeClassName={styles.Active}
+                                to="/">
+                                    <i className='fas fa-home'></i>Home
+                            </NavLink>
+                            {currentUser ? loggedInIcons : loggedOutIcons}
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
 }
 
 export default NavBar
